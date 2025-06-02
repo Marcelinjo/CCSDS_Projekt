9cc7befb69d16391b1bcb14084b315709786bf8b
Marcelinjo <Marcel.Beck03@gmx.de>
2025-06-02 16:13:24 +0200

    Minor changes to Header_Generator due to Architecture Change

--- SourceCode/CCSDS_Stack_Projekt.srcs/sources_1/new/Header_Generator.vhd
+++ SourceCode/CCSDS_Stack_Projekt.srcs/sources_1/new/Header_Generator.vhd
@@ -15,11 +15,11 @@ architecture Behavioral of HeaderGenerator is
     signal Transfer_Frame_Version_Number: STD_LOGIC_VECTOR(1 downto 0) := "00";                     -- shall be set to 0 according to CCSDS 132.0-B-3 Chapter 4.1.2.2.2
     signal SCID: STD_LOGIC_VECTOR(9 downto 0) := "0000000000";
     signal VCID: STD_LOGIC_VECTOR(2 downto 0) := "000";                                             -- Virtual Channel set to Channel 0
-    signal OCF_Flag: STD_LOGIC := '0';                                                              -- OCF not present, thus '0'
+    signal OCF_Flag: STD_LOGIC := '1';                                                              -- OCF present, thus '1'
     signal Master_Channel_Frame_Count: STD_LOGIC_VECTOR(7 downto 0) := "00000000";                  -- Counts the number of Master Frames Transfered
     signal Virtual_Channel_Frame_Count: STD_LOGIC_VECTOR(7 downto 0) := "00000000";                 -- Counts the number of Virtual Frames Transfered
     
-    signal Transfer_Frame_Secondary_Header_Flag: STD_LOGIC := '0';                                  -- Secondary Header is not present, thus Flag shall be 0 according to CCSDS 132.0-B-3 Chapter 4.1.2.7.2 
+    signal Transfer_Frame_Secondary_Header_Flag: STD_LOGIC := '1';                                  -- Secondary Header is present, thus Flag shall be 1 according to CCSDS 132.0-B-3 Chapter 4.1.2.7.2 
     signal Synchronization_Flag: STD_LOGIC := '0';                                                  -- Synchronization Flag shall signal ctet-synchronized and forward-ordered Packets or Idle Data are inserted according to CCSDS 132.0-B-3 Chapter 4.1.2.7.3  
     signal Packet_Order_Flag: STD_LOGIC := '1';                                                     -- Packet Order Flag as Order is undefined according to CCSDS 132.0-B-3 Chapter 4.1.2.7.4
     signal Segment_Length_Identifier: STD_LOGIC_VECTOR(1 downto 0) := "00";                         -- Segment Length Identifier is undefined according to CCSDS 132.0-B-3 Chapter 4.1.2.7.5
